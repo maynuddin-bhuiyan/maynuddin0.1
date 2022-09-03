d18f930514d6b145f48b809de728397d7d1068ac
uxmain <bhuiyanmayn@gmail.com>
2022-09-04 02:28:30 +0600

    first commit

---
+++ Component/Shared/Navbar/Navbar.js
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import React from 'react';
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+    return (
+        <div className={styles.Navbar}>
+<nav className="navbar navbar-expand-lg navbar-light" id={styles.Navbar}>
+  
+  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className={`collapse navbar-collapse justify-content-between ${styles.NavbarMenu}`} id="navbarToggleExternalContent">
+  <a className="navbar-brand" href="#">
+    <Image width="153" height="31" src="/Logo.png" />
+  </a>
+    <div className={`my-2 my-lg-0 ${styles.MenuItam}`}>
+    <ul className={`navbar-nav mr-auto ${styles.Menu}`}>
+      <li className="nav-item active">
+        <a className="nav-link" href="#">About <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Protfolio</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Experience</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="#">Contact</a>
+      </li>
+
+      <button className={styles.NavbarBtn}> 
+        <a href="https://drive.google.com/file/d/1_KIuP5xv7mjAucMsF99gi8nivWlUJVsS/view?usp=sharing" target="_blank" rel="noreferrer">Resume</a>
+      </button>
+      
+     
+    </ul> 
+    </div>
+  </div>
+</nav>
+</div>
+    );
+};
+
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import Script from "next/script";
+// import "slick-carousel/slick/slick-theme.css";
+// import "slick-carousel/slick/slick.css";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Maynuddin Bhuiyan</title>
+        <meta name="description" content="Designed & Built by BeeDesign" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin
+        ></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Livvic:wght@400;500;600;700;900&display=swap"
+          rel="stylesheet"
+        ></link>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
+          crossOrigin="anonymous"
+        ></link>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+        crossorigin="anonymous"
+      ></Script>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
